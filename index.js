0a3db0313d19464c5b367197c88da9eaba9e29ed
Joachim Schirrmacher <joachim.schirrmacher@deutschebahn.com>
2018-08-29 17:51:48 +0200

    Added functionality to update GitLab repo when changes on GitHub repo were made

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const HttpsProxyAgent = require('https-proxy-agent')
-const { URL } = require('url')
 const fs = require('fs')
+const { exec } = require('child_process')
 
 const secretsDir = process.env.SECRETSDIR || '/mnt/secrets';
 const pems = fs.readdirSync(secretsDir).filter(f => f.endsWith('.pem'))
@@ -18,8 +18,18 @@ const gitlabListener = require('./gitlab-listener')({port: process.env.GITLAB_PO
 const githubSender = require('./github-sender')({agent, app_id: process.env.GITHUB_APP_ID, installation_id: process.env.GITHUB_INST_ID, pem})
 const mapper = require('./mapper')(process.env.GITHUB_OWNER, process.env.GITHUB_REPO)
 
-githubListener.on('*', ({id, name, payload}) => {
+githubListener.on('push', ({id, name, payload}) => {
     console.log(name, 'event received from github', payload)
+    exec(`git clone https://github.com/${process.env.GITHUB_OWNER}/${process.env.GITHUB_REPO} tmp &&
+        cd tmp &&
+        git remote add gitlab ${process.env.GITLAB_BASE}${process.env.GITLAB_GROUP}/${process.env.GITLAB_REPO}.git &&
+        git push gitlab &&
+        cd .. &&
+        rm -rf tmp`, (err, stdout, stderr) => {
+            console.log(stdout)
+            console.error(stderr)
+        }
+    )
 })
 
 gitlabListener.on('build', async ({payload}) => {
